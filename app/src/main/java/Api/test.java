218b038f633587b98f7b13b11ddb0590d8b76a59
nikhil dhyani <nikhildhyani365@gmail.com>
2018-06-21 01:18:54 +0530

    .

--- app/src/main/java/Api/test.java
+++
@@ -1,8 +0,0 @@
-package Api;
-
-/**
- * Created by NIKHIL on 21-06-2018.
- */
-
-public class test {
-}
---
+++ app/src/main/java/com/example/nikhil/githubrepo/test2/javatest3.java
@@ -0,0 +1,8 @@
+package com.example.nikhil.githubrepo.test2;
+
+/**
+ * Created by NIKHIL on 21-06-2018.
+ */
+
+public class javatest3 {
+}
